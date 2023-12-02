5ab078f10b5a3660c5fa6fd81b888f4921be304f
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2023-12-02 22:33:26 +0700

    upload new project'

---
+++ java_jsvl/src/main/java/java_anotation/demo_4.java
@@ -0,0 +1,19 @@
+package java_anotation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = {"/demo4"})
+public class demo_4 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pr = resp.getWriter();
+		pr.println("<h1>Linh Nguyen</h1>");
+	}
+}
