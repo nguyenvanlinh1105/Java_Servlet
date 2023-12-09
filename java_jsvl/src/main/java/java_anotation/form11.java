ef828b08eff2b35166e0b67dd793b241fd6c93d7
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2023-12-10 00:22:09 +0700

    upload new file

---
+++ java_jsvl/src/main/java/java_anotation/form11.java
@@ -0,0 +1,23 @@
+package java_anotation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/html11")
+public class form11 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pr = resp.getWriter();
+		pr.println("<form action='/java_jsvl/demo11' methob='post'>");
+		pr.println("ten: <input type = 'text' name 'ten'>");
+		pr.println("tuoi: <input type = 'text' name 'tuoi'>");
+		pr.println("diaChi: <input type = 'text' name 'diaChi'>");
+		pr.println("<form action='/java_jsvl/demo11' methob='post'>");
+	}
+}
